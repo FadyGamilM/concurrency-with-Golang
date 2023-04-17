33ffb57cdc9c80c9f6097f21edc70bfc1e897c00
fady <gamilfady605@gmail.com>
2023-04-17 21:33:21 +0200

    channels have solved my missing join point

--- main.go
+++ main.go
@@ -5,11 +5,23 @@ import (
 )
 
 func main() {
-	go getData()
+	// define the communication channel
+	communicationChannel := make(chan string)
+
+	// call the routine
+	go getData(communicationChannel)
+
+	// wait for joining all forked routines
+	response := <-communicationChannel
+
+	fmt.Println("response is => ", response)
+
 	fmt.Println("processing is finished")
 }
 
-func getData() {
-	fmt.Println("data ..")
+func getData(communicationChannel chan string) {
+	fmt.Println("Doing some work ...")
+
+	// join the main routine
+	communicationChannel <- "done"
 }
- 
